154cb398f45705dcd3e5a2fedcc6bae69eaec2c0
olamideabiodun <olamidesokunbi15@gmail.com>
2023-02-16 16:22:42 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always
+ *main - Entry
+ *Return: Always
  */
 int main(void)
-{
-        puts("\"Programming is like building a multilingual puzzle");
+{	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
