59d45bd919697e1d0ee7c5d73633c1792736b4bb
olamideabiodun <olamidesokunbi15@gmail.com>
2023-02-17 16:53:56 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,17 @@
  */
 int main(void)
 {
-	char low;
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-	for (low = 'A'; low <= 'Z'; low++)
-		putchar(low);
-		putchar('\n');
+	{
+		if (low != e && low != q)
+			putchar(low);
+	}
+		printf("\n");
 
 	return (0);
 }
