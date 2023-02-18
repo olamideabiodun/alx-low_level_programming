d69fadc0b994e32c32001b33467cc1edd57ec9f7
olamideabiodun <olamidesokunbi15@gmail.com>
2023-02-17 16:16:04 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <string.h>
 #include <unistd.h>
 /**
-* main - Entry point
-* return 0;
- */
+ *main - prints a string and standard error
+ *Return: 1
+*/
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
