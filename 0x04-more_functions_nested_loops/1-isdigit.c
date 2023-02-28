e3ba7e038b86c220a3061973dee8db8cc9f6df06
olamideabiodun <olamidesokunbi15@gmail.com>
2023-02-28 12:48:55 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-isdigit.c -o 1-isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - check for a digit (0 through 9)
+ * _isdigit - check for a digit (0 through 9)
  * @c: input
  * Return: 1 if c is a digit & 0 otherwise
  */
 
 int _isdigit(char c)
 {
-	if ((c >= 0) && (c <= 9))
+	if ((c >= '0') && (c <= '9'))
 		return (1);
 	else
 		return (0);
