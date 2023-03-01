f6e0e76040e845ebcf258882b90132e2bda4809b
olamideabiodun <olamidesokunbi15@gmail.com>
2023-03-01 05:28:12 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 3-main.c 3-print_numbers.c -o 3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - a function that prints the numbers, from 0 to 9, followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i + '0');
+	putchar('\n');
+}
+
