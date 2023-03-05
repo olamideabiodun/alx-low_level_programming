898f5e8724adba91e752620b23b53c79c923a5b6
olamideabiodun <olamidesokunbi15@gmail.com>
2023-03-05 05:10:39 -0800

    ./4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - prints in reverse
+ *
+ * @s: string to print
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+		putchar(s[c]);
+	}
+
+	putchar('\n');
+}
