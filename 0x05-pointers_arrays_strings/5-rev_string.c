9df767318eeadb05408bf9f5d83ce424d7ce7e62
olamideabiodun <olamidesokunbi15@gmail.com>
2023-03-05 05:22:55 -0800

    cvbn

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string to reverse
+ *
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int i, c, k;
+	char *a, aux;
+
+	a = s;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (k = 1; k < c; k++)
+	{
+		a++;
+	}
+
+	for (i = 0; i < (c / 2); i++)
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = aux;
+		a--;
+	}
+}
+
