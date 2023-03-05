4c9b3793bd86e1aeb457768f124d9d176343dd6e
olamideabiodun <olamidesokunbi15@gmail.com>
2023-03-05 05:41:10 -0800

    im tired

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - prints any other character of a string
+ *
+ * @str: char to print
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+		putchar(str[i]);
+	}
+
+	putchar('\n');
+}
