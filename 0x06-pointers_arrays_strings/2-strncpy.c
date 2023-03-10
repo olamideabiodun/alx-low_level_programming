b182bb65e5998d8fd46dbe6299d74a433bd34416
olamideabiodun <olamidesokunbi15@gmail.com>
2023-03-10 06:13:42 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-strncpy.c -o 2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncpy - Copies at most an inputted number
+ *            of bytes from string src into dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copied from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+
+	while (*src != '\0' && n-- > 0)
+	{
+		*ptr++ = *src++;
+	}
+	while (n-- > 0)
+	{
+		*ptr++ = '\0';
+	}
+	return (dest);
+}
