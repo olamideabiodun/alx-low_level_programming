e0527841645371ede5033e1ad26a9aca061f37aa
olamideabiodun <olamidesokunbi15@gmail.com>
2023-03-19 06:00:07 -0700

    ofesumi nisha

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,15 +12,19 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *arr;
+	unsigned int i;
+
 	if (size == 0)
 		return (NULL);
 
-	char *arr = malloc(sizeof(char) * size);
+	arr = malloc(sizeof(char) * size);
 
 	if (arr == NULL)
 		return (NULL);
 
-	for (unsigned int i = 0; i < size; i++)
+
+	for (i = 0; i < size; i++)
 		arr[i] = c;
 
 	return (arr);
