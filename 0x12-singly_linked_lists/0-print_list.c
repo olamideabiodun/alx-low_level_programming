9deeba23e5e8016f4f416f85ed5d08188e3b88a3
root <olamidesokunbi15@gmail.com>
2023-07-27 16:30:12 -0700

    singky linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+
+/**
+ * print_list - prints elements
+ * @h: pointer to the head
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count  = 0;
+
+	while (h != NULL)
+	{
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+	{
+		printf("[%u} %s\n", h->len, h->str);
+	}
+	h = h->next;
+	count++;
+	}
+	return (count);
+}
+
