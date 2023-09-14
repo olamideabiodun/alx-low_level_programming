36338761b763334170327c727f5f70cb598eda99
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2023-09-14 20:07:36 +0300

    add node

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,7 +7,8 @@
 size_t print_dlistint(const dlistint_t *h){
     size_t count;
     count = 0;
-    if (h == NULL){
+    if (h == NULL)
+    {
         return (count);
     }
     while (h != NULL)
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * add_dnodeint - add node
+ * @head: list head
+ * @n: value
+ * Return:  the address of the new element, or NULL if it failed
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+    dlistint_t *new;
+    dlistint_t *h;
+    new = malloc(sizeof(dlistint_t));
+    if (new == NULL)
+    {
+        return (NULL);
+    }
+    new->n = n;
+    new->prev = NULL;
+    h = *head;
+    if (h != NULL)
+    {
+        while (h->prev != NULL)
+    {
+        h = h->prev;
+    }
+    }
+    new->next = h;
+    if (h != NULL)
+		h->prev = new;
+
+	*head = new;
+    return (new);
+}
