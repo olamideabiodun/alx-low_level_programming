2e203b8853b925596a9fe4e31663148fb23403b3
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2023-09-14 19:47:13 +0300

    list count

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,17 +5,11 @@
  * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h){
-    int count;
+    size_t count;
     count = 0;
     if (h == NULL){
         return (count);
     }
-
-    while (h->prev != NULL)
-    {
-        h = h->prev;
-    }
-
     while (h != NULL)
     {
         printf("%d\n", h->n);
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,16 +6,12 @@
 */
 size_t dlistint_len(const dlistint_t *h)
 {
-    int count = 0;
-    if (h == NULL)
-    {
-        return (count);
-    }
+    size_t count = 0;
     while (h->prev != NULL)
     {
         h = h->prev;
     }
-    while (h->next != NULL)
+    while (h != NULL)
     {
         count++;
         h = h->next;
