c1fd3cb8d566885131de8709df2ea0ebb7df45cd
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2023-09-14 21:18:40 +0300

    free node

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,9 +7,13 @@ void free_dlistint(dlistint_t *head)
 {
     dlistint_t *h;
 
-    while (head != NULL)
+if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+
+    while ((h = head) != NULL)
     {
-        h = head;
         head = head->next;
     free(h);
+    }
 }
