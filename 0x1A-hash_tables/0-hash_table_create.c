3149bb6524c5ee221f137d44930e6922c8322b31
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2023-12-01 23:57:43 +0200

    ughhh

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,8 +6,7 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    //return null if the hash table size is 0
-    hash_table_t *new_table;
+    hash_table_t *new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
     hash_node_t **array;
     unsigned long int i;
     if (size == 0)
@@ -15,26 +14,21 @@ hash_table_t *hash_table_create(unsigned long int size)
         return NULL;
 
     }
-    // allocates memory for the new table
-   
-    new_table = malloc(sizeof(hash_table_t));
-    // Checks if new table is null (if malloc failed)
     if (new_table == NULL)
     {
         return NULL;
     }
-    array = malloc(sizeof(hash_node_t *) * size);
+    array = (hash_node_t **)calloc(new_table->size, sizeof(hash_node_t *));
     if (array == NULL)
     {
         return NULL;
     }
-    // initializing elements of array to null
+
     for (i = 0; i < size; i++)
     {
         array[i] = NULL;
     }
     new_table->array = array;
 	new_table->size = size;
-    // return new table
     return (new_table);
 }
