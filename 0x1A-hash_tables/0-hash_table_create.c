b6a7a8aad9d186ae1077bee8512d2dcc050a5108
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2023-12-01 23:20:32 +0200

    initializes ele array to null

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
     //return null if the hash table size is 0
     hash_table_t *new_table;
+    unsigned long int i;
     if (size == 0)
     {
         return NULL;
@@ -15,19 +16,23 @@ hash_table_t *hash_table_create(unsigned long int size)
     }
     // allocates memory for the new table
    
-    new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+    new_table = malloc(sizeof(hash_table_t));
     // Checks if new table is null (if malloc failed)
     if (new_table == NULL)
     {
         return NULL;
     }
     new_table->size = size;
-    new_table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
+    new_table->array = malloc(sizeof(hash_node_t *) * size);
     if (new_table->array == NULL)
     {
         // if calloc failed, free memory and return Null
         free(new_table);
         return NULL;
+    }
+    for (i = 0; i < size; i++)
+    {
+        new_table->array[i] = NULL;
     }
     // return new table
     return (new_table);
